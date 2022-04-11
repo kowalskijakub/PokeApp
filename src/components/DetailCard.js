420af14a68351bdb44f8e6cf5009ce9e5fd2b517
Jakub <kowalskijakub95@gmail.com>
2022-04-11 15:47:19 +0200

    Display information in tables in DetailCard

--- src/components/DetailCard.js
+++ src/components/DetailCard.js
@@ -3,6 +3,7 @@ import './DetailCard.css';
 import Loading from './Loading';
 const InfoCard = ({ detailInfo, setVisibilityDetailCard }) => {
   const [detail, setDetail] = useState('');
+
   useEffect(() => {
     fetch(detailInfo[1].url)
       .then(response => response.json())
@@ -32,6 +33,29 @@ const InfoCard = ({ detailInfo, setVisibilityDetailCard }) => {
           return '';
         }
       });
+    const renderedStats = detail.stats.map(({ base_stat, stat }) => {
+      return (
+        <tr className="stat" key={stat.url}>
+          <td>{stat.name.split('-').join(' ').toUpperCase()}</td>
+          <td>{base_stat}</td>
+        </tr>
+      );
+    });
+    const renderedAbilities = detail.abilities.map(({ ability, slot }) => {
+      return (
+        <tr className="ability" key={ability.url}>
+          <td>{ability.name.split('-').join(' ').toUpperCase()}</td>
+          <td>{slot}</td>
+        </tr>
+      );
+    });
+    const renderedTypes = detail.types.map(({ type }) => {
+      return (
+        <tr>
+          <td>{type.name}</td>
+        </tr>
+      );
+    });
     return (
       <div className="info_container">
         <div className="information">
@@ -47,6 +71,37 @@ const InfoCard = ({ detailInfo, setVisibilityDetailCard }) => {
           <div className="detail">
             <h1>{detail.name}</h1>
             <div className="SpriteContainer">{renderedSprites}</div>
+            <div className="detail_flex">
+              <div className="Types">
+                <h3>TYPES</h3>
+                <table>
+                  <tr>
+                    <th>NAME</th>
+                  </tr>
+                  {renderedTypes}
+                </table>
+              </div>
+              <div className="abilities">
+                <h3>ABILITIES</h3>
+                <table>
+                  <tr>
+                    <th>NAME</th>
+                    <th>SLOT</th>
+                  </tr>
+                  {renderedAbilities}
+                </table>
+              </div>
+              <div className="stats">
+                <h3>STATISTICS</h3>
+                <table>
+                  <tr>
+                    <th>NAME</th>
+                    <th>BASE</th>
+                  </tr>
+                  {renderedStats}
+                </table>
+              </div>
+            </div>
           </div>
         </div>
       </div>
