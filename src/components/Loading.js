9f637938875c1db0a791d0703b9ed0af7bae5889
jakub <kowalskijakub95@gmail.com>
2022-04-12 15:06:22 +0200

    Displaying refresh information in Loading.js after 4 seconds

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Loading.css';
 const Loading = () => {
+  const [show, setShow] = useState(false);
+  useEffect(() => {
+    setTimeout(() => {
+      setShow(true);
+    }, 4000);
+  }, []);
   return (
     <div className="loading_page">
       <div className="loading_text">
         <h1>Loading</h1>
       </div>
+      <p className={show ? `failed_load` : `failed_load close_failed_load`}>
+        If it takes too long to load, please refresh the page!
+      </p>
     </div>
   );
 };
